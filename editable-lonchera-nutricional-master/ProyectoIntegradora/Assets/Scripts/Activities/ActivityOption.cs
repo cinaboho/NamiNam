f3f0730304d821b1dffa1c5c07d1263fa92fa8ff
cinaboho <cinaboho@espol.edu.ec>
2020-08-18 21:54:47 -0500

    Activity
    Activity Controller

---
+++ editable-lonchera-nutricional-master/ProyectoIntegradora/Assets/Scripts/Activities/ActivityOption.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivityOption : MonoBehaviour {
+
+    public bool isAlive;
+    public bool selected;
+    
+}
