d95828802e9b5c9c6831c0d0949224372e6cf541
cinaboho <cinaboho@espol.edu.ec>
2020-08-18 23:25:02 -0500

    Session Manager

---
+++ editable-lonchera-nutricional-master/ProyectoIntegradora/Assets/Scripts/SessionManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+using System.IO;
+
+[System.Serializable]
+public class SessionManager : UnitySingleton<SessionManager> {
+
+    //private string school = "ES001";
+    //private string room = "R1";
+    
+    //public string PlayerAvatarName { get; set; }
+    //public string PlayerName { get; set; }
+    
+
+    public string tipo = "jugador";
+    public string avatar;
+    public string nombre_jugador;
+    public string nombre_juego = "En mi Entorno Natural";
+    private string V;
+
+    
+    private int[] playerScore = { 0, 0, 0, 0 };
+
+    public int[] getPlayerScore()
+    {
+        return this.playerScore;
+    }
+
+    public void setPlayerScore(int[] score)
+    {
+        this.playerScore = score;
+    }
+    
+
+
+    public void SetPlayerInfo(string avatar, string nombre_jugador)
+    {
+        string school = GameStateManager.Instance.GetGlobalSettings().school;
+        string room = GameStateManager.Instance.GetGlobalSettings().room;
+
+        if (school == "" || room == "")
+        {
+            V = "";
+        }
+        else
+        {
+            V = "-";
+        }
+
+        this.avatar = avatar;
+        this.nombre_jugador = school + V + room + V + nombre_jugador;
+
+        GameStateManager.Instance.AddJsonToList(JsonUtility.ToJson(this));
+    }
+
+}
+
+[Serializable]
+public class GlobalSettings
+{
+    public string school;
+    public string room;
+
+    public GlobalSettings()
+    {
+        school = "";
+        room = "";
+    }
+
+    public GlobalSettings(string school, string room)
+    {
+        this.school = school;
+        this.room = room;
+    }
+
+}
